62f0e1f01c40109717ffff409cd710f0a7f74f59
ZhenyaVal <evg-meldin@ya.ru>
2016-12-01 19:27:57 +0300

    Good AVL output and text.

--- AVLTree.h
+++ AVLTree.h
@@ -249,7 +249,7 @@ public:
 		char ch[2] = { 0 };
 		int x = (x1 + x2) / 2;
 		int i = 0;
-		ch[0] = leaf->key;
+		ch[0] = Ttochar(leaf->key);
 		moveto(x, y);
 		outtext(ch);
 		if (PaintLeafBT(x1 - 5, x, y + 20, leaf->left, level - 5, 'l')) //Рисуем левую ветвь
@@ -262,6 +262,30 @@ public:
 		}
 		return true;
 	}
+	template <typename T>	char Ttochar(T x) //Функция перевода в символ
+{
+	int r = x % 10;
+	switch (r)
+	{
+	case 1: return '1';
+	case 2: return '2';
+	case 3: return '3';
+	case 4: return '4';
+	case 5: return '5';
+	case 6: return '6';
+	case 7: return '7';
+	case 8: return '8';
+	case 9: return '9';
+	default: return '0';
+		break;
+	}
+}
+~AVLTree()
+{
+	delTree(main_root);
+}
+
+// Дополнительные функции (если нужно прицеплять готовое решение)
 	~AVLTree()
 	{
 		delTree(main_root);
